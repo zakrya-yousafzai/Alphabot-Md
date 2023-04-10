956aeca7439cdaee066cbf7d414264c570131043
zakrya-yousafzai <76619729+zakrya-yousafzai@users.noreply.github.com>
2023-04-10 22:37:53 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,17 +32,17 @@ global.language = english //change indonesia to english if you don't understand 
 // Other
 // Other
 global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.ownername = "zakrya khan" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
 global.email = "mdsandaru2@gmail.com" //bebas
-global.region = "Buddhist" //bebas
-global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.region = "Pakistan" //bebas
+global.timezone = 'Asia/Islamabad' //  timezone wib
+global.premium = ['923189079020'] //premium user
+global.owner = ["3189079020", "923189079020"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "3189079020" // nomor wa kalian
+global.ownernomerr = "+923189079020" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
